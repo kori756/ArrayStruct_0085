1039ce722e618da26c2afad99dc39e45666ccbf4
kori756 <khorirachmatr@gmail.com>
2026-04-07 15:59:53 +0700

    menambahkan program utama

--- structinstruct.cpp
+++ structinstruct.cpp
@@ -11,3 +11,23 @@ struct orang{
     int umur;
 };
 
+int main()
+{
+    //deklarasi obyek dari strucct orang
+    orang mhs;
+    //isi obyek
+    cout << "nama mahasiswa : " ;
+    cin >> mhs.nama;
+    cout << "alamat desa : " ;
+    cin >> mhs.alamat.desa;
+    cout << "alamat kota : " ;
+    cin >> mhs.alamat.kota;
+    cout << "umur : " ;
+    cin >> mhs.umur;
+    cout << endl;
+    //menampilkan isi obyek
+    cout << "nama : " << mhs.nama<<endl;
+    cout << "desa : " << mhs.alamat.desa<<endl;
+    cout << "kota : " << mhs.alamat.kota<<endl;
+    cout << "umur : " << mhs.umur<<endl;
+}
