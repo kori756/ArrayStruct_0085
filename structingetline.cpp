584132fd4525405256a9c1547b90910b61388063
kori756 <khorirachmatr@gmail.com>
2026-04-07 16:03:48 +0700

    menambahkan program utama

--- structingetline.cpp
+++ structingetline.cpp
@@ -11,3 +11,23 @@ struct orang{
     int umur;
 };
 
+int main()
+{
+    //deklarasi obyek dari strucct orang
+    orang mhs;
+    //isi obyek
+    cout << "nama mahasiswa : " ;
+    getline(cin, mhs.nama);
+    cout << "alamat desa : " ;
+    getline(cin, mhs.alamat.desa);
+    cout << "umur : " ;
+    getline(cin, mhs.alamat.kota);
+    cout << "umur : ";
+    cin >> mhs.umur;
+    cout << endl;
+    //menampilkan isi obyek
+    cout << "nama : " << mhs.nama<<endl;
+    cout << "desa : " << mhs.alamat.desa<<endl;
+    cout << "kota : " << mhs.alamat.kota<<endl;
+    cout << "umur : " << mhs.umur<<endl;
+}
