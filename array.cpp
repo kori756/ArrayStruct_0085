ae7a0d3bc0aa9a40a60033b62c2d5fdd2aeffd64
kori756 <khorirachmatr@gmail.com>
2026-04-07 15:53:38 +0700

    mengisi array

--- array.cpp
+++ array.cpp
@@ -16,4 +16,16 @@ int main(){
     dataku[2] = 600;
     cout << "Data ke - 3 yang baru : " << dataku[2]<<endl;
 
+    //meengisi array datamu satu persatu
+    cout << "Data index ke - 0 :" ;
+    cin >> datamu[0];
+    cout << "Data index ke - 1 :" ;
+    cin >> datamu[1];
+    cout << "Data index ke - 2 :" ;
+    cin >> datamu[2];
+    cout << "Data index ke - 3 :" ;
+    cin >> datamu[3];
+    cout << "Data index ke - 4 :" ;
+    cin >> datamu[4];
+
     
